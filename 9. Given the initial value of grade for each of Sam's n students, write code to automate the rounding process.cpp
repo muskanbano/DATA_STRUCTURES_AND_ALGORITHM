284953ab8032423a07ffba283a3d9251c280b7df
MUSKAN BANO <bmuskan011@gmail.com>
2021-09-05 10:34:35 +0530

    Update 9. Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.cpp

--- 9. Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.cpp
+++ 9. Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.cpp
@@ -1,4 +1,4 @@
-![CLICK HERE FOR QUESTION](https://www.hackerrank.com/challenges/grading/problem)
+!CLICK HERE FOR QUESTION(https://www.hackerrank.com/challenges/grading/problem)
 
 #include <bits/stdc++.h>
 
